40ca307348ba8a9a447383af0add1fa15cf14ab0
foxunderwater <32735072+foxunderwater@users.noreply.github.com>
2019-02-08 09:01:43 +0700

    backpress like home button press

--- android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
@@ -196,7 +196,8 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
     @Override
     public void invokeDefaultOnBackPressed() {
         if (layout != null && !layout.onBackPressed()) {
-            super.onBackPressed();
+            //super.onBackPressed();
+			this.moveTaskToBack(true);
         }
     }
 
